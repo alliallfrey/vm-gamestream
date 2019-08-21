cc7c6b361961ff5e92a8918308ea3664c593c0c6
Alli Allfrey <alastair.allfrey@ocsl.co.uk>
2019-08-21 11:51:03 +0100

    Run Chrome installer using Start-Process
    Deployment errors during template deployment:
     - Chrome installer moved to Start-Process command
    - Blizzard launcher cannot be downloaded using
     Invoke-WebRequest

--- vm-gamestream-cse.ps1
+++ vm-gamestream-cse.ps1
@@ -4,13 +4,13 @@ $path = (New-Item -Path "C:\" -Name "gamestream-setup-files" -ItemType "Director
 
 # Download and install Chrome
 Invoke-WebRequest -uri "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "$path\ChromeSetup.exe" -ErrorAction SilentlyContinue
-"$path\ChromeSetup.exe" /silent /install
+Start-Process -FilePath $path\ChromeSetup.exe -Args "/silent /install"
 
 # Download installers/ driver
 ## Parsec
 Invoke-WebRequest -uri "https://s3.amazonaws.com/parsec-build/package/parsec-windows.exe" -OutFile "$path\parsec-windows.exe" -ErrorAction SilentlyContinue
-## Battlenet
-Invoke-WebRequest -uri "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP" -OutFile "$path\Battle.net-Setup.exe" -ErrorAction SilentlyContinue
+## Battlenet, currently does not work via Invoke-WebRequest
+# Invoke-WebRequest -uri "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP" -OutFile "$path\Battle.net-Setup.exe" -ErrorAction SilentlyContinue
 ## Virtual Audio Cable
 Invoke-WebRequest -uri "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip" -OutFile "$path\VBCABLE_Driver_Pack43.zip" -ErrorAction SilentlyContinue
 ## NVIDIA GRID Driver
